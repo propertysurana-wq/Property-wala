91f840936b2e3b05a7366cd7229fca28b6551794
agnipath-00 <189tobx@mydefipet.live>
2026-01-15 10:27:36 +0530

    frontend updates in dev branch

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,45 @@
+// // import type { NextConfig } from "next";
+
+// // const nextConfig: NextConfig = {
+// //   /* config options here */
+// //   reactCompiler: true,
+// // };
+
+// // export default nextConfig;
 // import type { NextConfig } from "next";
 
 // const nextConfig: NextConfig = {
 //   /* config options here */
 //   reactCompiler: true,
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'images.unsplash.com',
+//         port: '',
+//         pathname: '/**',
+//       },
+//     ],
+//     qualities: [75, 85],
+//   },
 // };
 
 // export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  turbopack: {
+    root: __dirname, // ✅ warning fix
+  },
+
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
       },
     ],
     qualities: [75, 85],
